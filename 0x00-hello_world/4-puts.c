d56d651e0a45c3f49eec9adadbb852e77215609c
emmanuelobiagbaoso <wingod914@gmail.com>
2023-02-16 16:07:31 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * *  *main- A program that uses puts to print a string
  *   *Return: ends with 0
- *    */
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
