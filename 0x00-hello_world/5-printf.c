4423376f0b1d775364c4287932c0f8e6c88379e8
emmanuelobiagbaoso <wingod914@gmail.com>
2023-02-16 16:18:32 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  *  * main- prints a string followed by a new line
  *   * Return: 0
- *   */
+ **/
 int main(void)
 {
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
